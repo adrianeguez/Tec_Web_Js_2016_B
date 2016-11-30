c7261c078030de6d2586183b5faae4844ee34312
Adrian Eguez <eadepto@hotmail.com>
2016-11-30 14:31:51 -0500

    Rest Usuarios Get 1 Get Todos

--- Servidor HTTP/app.js
+++ Servidor HTTP/app.js
@@ -4,8 +4,46 @@ var app = express();
 
 var puerto = 6060;
 
+var usuarios = [
+    {
+        id:1,
+        nombre:'Pepe',
+        cedula:'123409182'
+    },
+    {
+        id:2,
+        nombre:'Carlos',
+        cedula:'981237918'
+    },
+    {
+        id:3,
+        nombre:'Juan',
+        cedula:'011283934'
+    }
+]
+
+
+
 app.get('/', function (req, res) {
     res.send('Hello World!')
+})
+
+app.get('/Usuario', function (req, res) {
+
+    res.json(usuarios);
+    
+})
+
+app.get('/Usuario/:idUsuario', function (req, res) {
+    
+    var idActual = req.params.idUsuario;
+    for(var i=0;i<usuarios.length;i++){
+        if(idActual == usuarios[i].id){
+            res.json(usuarios[i]);
+        }
+    }
+    res.send('No existe el Usuario');
+    
 })
 
 app.get('/TecnologiasWeb', function (req, res) {
@@ -20,14 +58,16 @@ app.post('/TecnologiasWeb', function (req, res) {
     
     var parametros = req.params;
     
+    console.log(parametros);
+    
     var usuario = {
         nombre:'Adrian',
         cedula:'000000000'
     }
     
-    usuario.apellido = '';
-    usuario.mascotas = [];
-    usuario.casado = false;
+    
+    res.append('token', '1234');
+    res.json(usuario);
     
     
 //    usuario = {
@@ -43,22 +83,18 @@ app.post('/TecnologiasWeb', function (req, res) {
     
     
     
-    console.log('Lo que tengo en el REQUEST es:')
-    console.log(req);
-    console.log('Lo que tengo en el RESPONSE es:')
-    console.log(res);
-    
-    console.log('Cabeceras Req:')
-    console.log(req.headers);
-    console.log('Cabeceras Res:')
-    console.log(res.headers);
-    
+//    console.log('Lo que tengo en el REQUEST es:')
+//    console.log(req);
+//    console.log('Lo que tengo en el RESPONSE es:')
+//    console.log(res);
+//    
+//    console.log('Cabeceras Req:')
+//    console.log(req.headers);
+//    console.log('Cabeceras Res:')
+//    console.log(res.headers);
+//    
 
-    res.append('token', '1234');
-    
-//    res.headers.token = 1234;
-    
-    res.json(usuario);
+  
     
 //    res.send('{nombre:Adrian,cedula:171813714}');
     
