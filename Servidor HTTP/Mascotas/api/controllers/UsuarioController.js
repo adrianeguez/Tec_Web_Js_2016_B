c3a94cc128fa8862973ab84897722386dbbe1a34
Adrian Eguez <eadepto@hotmail.com>
2017-01-10 14:55:46 -0500

    Relaciones en Sailsjs

--- Servidor HTTP/Mascotas/api/controllers/UsuarioController.js
+++ Servidor HTTP/Mascotas/api/controllers/UsuarioController.js
@@ -15,33 +15,27 @@ module.exports = {
         // Guardando todos los parametros en la variable parametros
 
         var parametros = req.allParams();
-
         console.log(parametros);
 
-        if (parametros.nombres && parametros.apellidos) {
-
-            //creo el usuario
-
-            Usuario.create({
-                nombres: parametros.nombres,
-                apellidos: parametros.apellidos,
-                correo: parametros.correo
-            }).exec(function (error, usuarioCreado) {
-                if (error) return res.serverError()
-
-                sails.log.info(usuarioCreado);
-
-                return res.ok(usuarioCreado);
-            });
-
-        } else {
-
-            // bad Request
-
-            return res.badRequest('No envia todos los parametros');
-
+        if (req.method == 'POST') {
+            if (parametros.nombres && parametros.apellidos) {
+                //creo el usuario
+                Usuario.create({
+                    nombres: parametros.nombres,
+                    apellidos: parametros.apellidos,
+                    correo: parametros.correo
+                }).exec(function (error, usuarioCreado) {
+                    if (error) return res.serverError()
+                    sails.log.info(usuarioCreado);
+                    return res.ok(usuarioCreado);
+                });
+            } else {
+                // bad Request
+                return res.badRequest('No envia todos los parametros');
+            }
+        }else{
+            return res.badRequest('Metodo invalido');
         }
-
 
     }
 
