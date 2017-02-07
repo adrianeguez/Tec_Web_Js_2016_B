8f714edc63117af16cb2c395ed4977f94967f529
Adrian Eguez <eadepto@hotmail.com>
2017-02-07 14:44:02 -0500

    Crear Usuario Validar Metodo POST

--- Mascotas/api/controllers/UsuarioController.js
+++ Mascotas/api/controllers/UsuarioController.js
@@ -10,81 +10,23 @@
 module.exports = {
 
     crearUsuario: function (req, res) {
-        
-        if(req.method=="POST"){
-            
-        }else{
-            
-        }
-        
-        
-        
-        
-        
-        
-        
 
-        var parametros = req.allParams();
-        console.log(parametros);
+        if (req.method == "POST") {
 
-        if (req.method == 'POST') {
-            if (parametros.nombres && parametros.apellidos) {
-                //creo el usuario
-                Usuario.create({
-                    nombres: parametros.nombres,
-                    apellidos: parametros.apellidos,
-                    correo: parametros.correo
-                }).exec(function (error, usuarioCreado) {
-                    if (error) return res.serverError()
-                    sails.log.info(usuarioCreado);
-                    return res.ok(usuarioCreado);
-                });
-            } else {
-                // bad Request
-                return res.badRequest('No envia todos los parametros');
-            }
         } else {
-            return res.badRequest('Metodo invalido');
+            
+            return res.view('vistas/Error',{
+            error:{
+                desripcion:"Error en el uso del Metodo HTTP",
+                rawError:"HTTP Invalido",
+                url:"/CrearUsuario"
+            }
+        });
+
         }
 
-    },
-//    crearUsuarioForm: function (req, res) {
-//
-//        var parametros = req.allParams();
-//        console.log(parametros);
-//
-//        if (req.method == 'POST') {
-//            if (parametros.nombres && parametros.apellidos) {
-//                //creo el usuario
-//                Usuario.create({
-//                    nombres: parametros.nombres,
-//                    apellidos: parametros.apellidos,
-//                    correo: parametros.correo
-//                }).exec(function (error, usuarioCreado) {
-//                    if (error) return res.serverError()
-//                    sails.log.info(usuarioCreado);
-//
-//                    return res.view('vistas/home', {
-//                        titulo: 'Inicio',
-//                        numero: 1,
-//                        mauricio: {
-//                            nombre: 'Mauricio',
-//                            cedula: 1718137159
-//                        }
-//                    });
-//                });
-//
-//
-//
-//
-//            } else {
-//                // bad Request
-//                return res.badRequest('No envia todos los parametros');
-//            }
-//        } else {
-//            return res.badRequest('Metodo invalido');
-//        }
-//
-//    }
-
+    }
 };
+
+
+
