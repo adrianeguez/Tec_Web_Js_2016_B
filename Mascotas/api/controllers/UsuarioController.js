8f7ea3e7c0b72d78cdf47fa76c27375939abd722
Adrian Eguez <eadepto@hotmail.com>
2017-02-07 15:07:47 -0500

    Crear Usuarios Fix correo

--- Mascotas/api/controllers/UsuarioController.js
+++ Mascotas/api/controllers/UsuarioController.js
@@ -13,20 +13,64 @@ module.exports = {
 
         if (req.method == "POST") {
 
-        } else {
-            
-            return res.view('vistas/Error',{
-            error:{
-                desripcion:"Error en el uso del Metodo HTTP",
-                rawError:"HTTP Invalido",
-                url:"/CrearUsuario"
+            var parametros = req.allParams();
+
+            if (parametros.nombres && parametros.apellidos) {
+
+                var usuarioCrear = {
+                    nombres: parametros.nombres,
+                    apellidos: parametros.apellidos,
+                    correo: parametros.correo
+                }
+
+                if (usuarioCrear.correo == "") {
+                    delete usuarioCrear.correo
+                }
+
+                Usuario.create(usuarioCrear).exec(function (err, usuarioCreado) {
+
+                    if (err) {
+                        return res.view('vistas/Error', {
+                            error: {
+                                desripcion: "Fallo al crear el Usuario",
+                                rawError: err,
+                                url: "/CrearUsuario"
+                            }
+
+                        });
+                    }
+
+                    return res.view('vistas/Usuario/crearUsuario');
+
+
+                })
+
+
+            } else {
+
+                return res.view('vistas/Error', {
+                    error: {
+                        desripcion: "Llena todos los parametros, apellidos y nombres",
+                        rawError: "Fallo en envio de parametros",
+                        url: "/CrearUsuario"
+                    }
+
+                });
+
             }
-        });
+
+
+        } else {
+
+            return res.view('vistas/Error', {
+                error: {
+                    desripcion: "Error en el uso del Metodo HTTP",
+                    rawError: "HTTP Invalido",
+                    url: "/CrearUsuario"
+                }
+            });
 
         }
 
     }
 };
-
-
-
