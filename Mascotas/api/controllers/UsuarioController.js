5324cc48f9ab96df21f88821ee9b9292c2556291
Adrian Eguez <eadepto@hotmail.com>
2017-02-08 14:16:00 -0500

    cambiar CrearUsuarios

--- Mascotas/api/controllers/UsuarioController.js
+++ Mascotas/api/controllers/UsuarioController.js
@@ -40,7 +40,31 @@ module.exports = {
                         });
                     }
 
-                    return res.view('vistas/Usuario/crearUsuario');
+
+
+
+                    Usuario.find()
+                        .exec(function (errorIndefinido, usuariosEncontrados) {
+
+                            if (errorIndefinido) {
+                                res.view('vistas/Error', {
+                                    error: {
+                                        desripcion: "Hubo un problema cargando los Usuarios",
+                                        rawError: errorIndefinido,
+                                        url: "/ListarUsuarios"
+                                    }
+                                });
+                            }
+
+                            res.view('vistas/Usuario/ListarUsuarios', {
+                                usuarios: usuariosEncontrados
+                            });
+                        })
+
+
+
+
+
 
 
                 })
