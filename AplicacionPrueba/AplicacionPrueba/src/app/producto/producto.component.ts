dcfb83884661f45767e094115a3a94def127c75d
Adrian Eguez <eadepto@hotmail.com>
2017-02-24 16:50:15 -0500

    Productos

--- AplicacionPrueba/AplicacionPrueba/src/app/producto/producto.component.ts
+++ AplicacionPrueba/AplicacionPrueba/src/app/producto/producto.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Http, Response} from "@angular/http";
+import {MasterURlService} from "../services/master-url.service";
 
 @Component({
   selector: 'app-producto',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductoComponent implements OnInit {
 
-  constructor() { }
+  private _parametros: any;
+  productos=[];
+
+  constructor(private _ActivatedRoute: ActivatedRoute,
+              private _http:Http,
+              private _masterURL:MasterURlService) {
+  }
 
   ngOnInit() {
+
+    this._ActivatedRoute
+      .params
+      .subscribe(parametros => {
+        this._parametros = parametros;
+        this._http.get(this._masterURL.url+'Producto?idTienda='+this._parametros.idTienda)
+          .subscribe(
+            (res:Response)=>{
+              this.productos = res.json();
+            },
+            (err)=>{
+              console.log(err)
+            }
+          )
+
+      });
+
   }
 
 }
