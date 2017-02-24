91c888eccf02224001dbaed62bd2a5c886e08d60
Adrian Eguez <eadepto@hotmail.com>
2017-02-24 16:33:26 -0500

    Rutas Productos

--- AplicacionPrueba/AplicacionPrueba/src/app/app.routes.ts
+++ AplicacionPrueba/AplicacionPrueba/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import {Routes, RouterModule} from '@angular/router';
 import { ModuleWithProviders }  from '@angular/core';
 import {HomeComponent} from "./home/home.component";
 import {TiendaComponent} from "./tienda/tienda.component";
+import {ProductoComponent} from "./producto/producto.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'tienda', component: TiendaComponent},
+  {path: 'tienda/:idTienda/producto', component: ProductoComponent}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
