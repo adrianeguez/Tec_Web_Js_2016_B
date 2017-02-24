b2fe8eeac93272599a2489410348b7780d2ab83e
Adrian Eguez <eadepto@hotmail.com>
2017-02-24 17:00:30 -0500

    productso crear

--- AplicacionPrueba/AplicacionPrueba/src/app/producto/producto.component.ts
+++ AplicacionPrueba/AplicacionPrueba/src/app/producto/producto.component.ts
@@ -9,10 +9,9 @@ import {MasterURlService} from "../services/master-url.service";
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent implements OnInit {
-
   private _parametros: any;
   productos=[];
-
+  nuevoProducto= {};
   constructor(private _ActivatedRoute: ActivatedRoute,
               private _http:Http,
               private _masterURL:MasterURlService) {
@@ -36,6 +35,21 @@ export class ProductoComponent implements OnInit {
 
       });
 
+  }
+  crearProducto(nombre:string){
+    let producto = {
+      nombre:nombre,
+      idTienda:this._parametros.idTienda
+    };
+    this._http.post(this._masterURL.url+'Producto',producto)
+      .subscribe(
+        (res:Response)=>{
+          this.productos.push(res.json());
+        },
+        (err)=>{
+          console.log(err)
+        }
+      )
   }
 
 }
